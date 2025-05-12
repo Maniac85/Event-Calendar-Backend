d23b19a7b2ad600bf3789fae705db51bece92234
Maniac85 <maniac-85@gmx.de>
2025-05-12 18:57:37 +0200

    M1 adjustments for M2

--- src/main/java/de/htwberlin/webtech/calendar/EventController.java
+++ src/main/java/de/htwberlin/webtech/calendar/EventController.java
@@ -1,5 +1,6 @@
 package de.htwberlin.webtech.calendar;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class EventController {
 
     @GetMapping("/events")
